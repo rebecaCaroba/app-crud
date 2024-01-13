8ba154f8f0cda4e851ca6cf15409a39aaab297a7
rebecaCaroba <rebecacaroba@gmail.com>
2024-01-13 10:00:24 -0300

    Obtendo dados do contato

--- src/components/ModalContact/index.tsx
+++ src/components/ModalContact/index.tsx
@@ -1,46 +1,90 @@
 'use client'
 import * as Dialog from '@radix-ui/react-dialog';
 import './style.scss';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
 
-export function ModalContact() {
+const newContactFormSchema = z.object({
+    name: z.string(),
+    email: z.
+    string()
+    .min(1, {message: 'Email inválido'})
+    .email('Email inválido'),
+    phone: z
+    .string()
+    .refine((value) => /\(\d{2}\)\s\d{4,5}-\d{4}/.test(value), {
+      message: 'Número de telefone inválido',
+    }),
+    type: z.enum(['M', 'F'])
+  })
+
+  type NewTransactionInputs = z.infer<typeof newContactFormSchema>
+
+  export function ModalContact() {
+  const {
+      register,
+      handleSubmit,
+      formState: { isSubmitting },
+    } = useForm<NewTransactionInputs>({
+      resolver: zodResolver(newContactFormSchema),
+    })
+
+  async function handleCreateNewContact(data: NewTransactionInputs) {
+    console.log(data)
+  }
+  
     return (
-        <Dialog.Portal>
-            <Dialog.Overlay>
-                <div className="overlay" />
-            </Dialog.Overlay>
-            <Dialog.Content>
-                <div className="content-modal">
-                    <Dialog.Title>Novo contato</Dialog.Title>
-                        <Dialog.DialogClose className="close-button">
-                            X
-                        </Dialog.DialogClose>
-                    <form>
-                        <input 
-                        type="text" 
-                        placeholder='Digite o nome' 
-                        required/>
-                        <input 
-                        type="email" 
-                        placeholder='exemple@email.com' 
-                        pattern=".+@example\.com" 
-                        required />
-                        <input 
-                        type="tel"
-                        placeholder='99999-9999' 
-                        required />
-
-                        <div className='radioContainer'>
-                            <label htmlFor="mas">Masculino</label>
-                            <input type="radio" name="sexo" id="mas" value="M" />
-                        </div>
-                        <div className='radioContainer'>
-                            <label htmlFor="fem">Feminino</label>
-                            <input type="radio" name="sexo" id="fem" value="F" />
-                        </div>
-                        <button type="submit">Adicionar</button>
-                    </form>
-                </div>
-            </Dialog.Content>
-        </Dialog.Portal>
+      <Dialog.Portal>
+        <Dialog.Overlay>
+          <div className="overlay" />
+        </Dialog.Overlay>
+        <Dialog.Content>
+          <div className="content-modal">
+            <Dialog.Title>Novo contato</Dialog.Title>
+            <Dialog.DialogClose className="close-button">X</Dialog.DialogClose>
+            <form onSubmit={handleSubmit(handleCreateNewContact)}>
+              <input
+                type="text"
+                placeholder="Digite o nome"
+                {...register('name')}
+                required
+              />
+  
+              <input
+                type="email"
+                placeholder="exemple@email.com"
+                {...register('email')}
+                required
+              />
+  
+              <input
+                type="text"
+                placeholder="(XX) XXXXX-XXXX"
+                {...register('phone')}
+                required
+              />
+              <div className="radioContainer">
+                <label htmlFor="mas">Masculino</label>
+                <input
+                  type="radio"
+                  id="mas"
+                  value="M"
+                  {...register('type', { required: 'Escolha uma opção' })}
+                />
+  
+                <label htmlFor="fem">Feminino</label>
+                <input
+                  type="radio"
+                  id="fem"
+                  value="F"
+                  {...register('type', { required: 'Escolha uma opção' })}
+                />
+              </div>
+              <button type="submit" disabled={isSubmitting}>Adicionar</button>
+            </form>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
     );
-}
+  }
